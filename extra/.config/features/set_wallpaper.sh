2ed39bed62955584beec76c239be354ea63345ed
moritz <moritz@oword.org>
2022-04-09 08:54:27 +0200

    Remove day night mode

--- extra/.config/features/set_wallpaper.sh
+++ extra/.config/features/set_wallpaper.sh
@@ -1,4 +1,8 @@
 function set_day_wallpaper() {
+  set_default_wallpaper
+}
+
+function set_default_wallpaper() {
   feh --bg-scale ~/Pictures/Wallpapers/mon1.jpg --bg-scale ~/Pictures/Wallpapers/mon2.jpg
 }
 
@@ -6,11 +10,13 @@ function set_night_wallpaper() {
   feh --bg-scale ~/Pictures/Wallpapers/night.jpg
 }
 
-hours=$(date +"%H")
+set_default_wallpaper
 
-if [ $hours -ge 9 -a $hours -lt 21 ]
-then
-  set_day_wallpaper
-else
-  set_night_wallpaper
-fi
+# hours=$(date +"%H")
+
+# if [ $hours -ge 9 -a $hours -lt 21 ]
+# then
+#   set_day_wallpaper
+# else
+#   set_night_wallpaper
+# fi
